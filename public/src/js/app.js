7988f6bf9d2db375f8afba8d9a1cd2083aa5bce7
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-07-13 15:16:12 -0600

    fetch event, remote debug, web app install banner at custom location

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,14 @@
+var deferredPrompt;
+
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('sw.js').then(function () {
 		console.log('service worker registered');
 	});
 }
+
+window.addEventListener('beforeinstallprompt', function (event) {
+	console.log('beforeinstallprompt fired');
+	event.preventDefault();
+	deferredPrompt = false;
+	return false;
+});
