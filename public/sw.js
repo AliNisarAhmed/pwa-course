3e1da88b3faa12a62920b95c1e826a69b6f501a4
Ali Ahmed <ali.ahmed@ama.ab.ca>
2021-07-23 09:22:54 -0600

    Versioning service worker caches

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,16 @@
-self.addEventListener('install', function (event) {
+var CACHE_STATIC_NAME = 'static-v3';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
+self.addEventListener('install', (event) => {
 	console.log('[Service Worker] installing Service worker ...', event);
+
 	event.waitUntil(
 		// waitUntil: dont finish the installation event until the given promise is done
-		caches.open('static').then(function (cache) {
+		caches.open(CACHE_STATIC_NAME).then(function (cache) {
 			console.log('[Service Worker] Precaching App Shell');
+			// cache.add and cache.addAll store both the key (request) and fetches the response for that request and stores it too
+			// vs
+			// cache.put, for which we need to provide what we want to store
 			cache.addAll([
 				'/',
 				'/index.html', // we are caching requests/url
@@ -17,28 +24,49 @@ self.addEventListener('install', function (event) {
 				'https://fonts.googleapis.com/icon?family=Material+Icons',
 				'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
 			]);
-			// cache.add('/');
-			// cache.add('/index.html');
-			// cache.add('/src/js/app.js');
 		})
 	);
 });
 
-self.addEventListener('activate', function (event) {
+self.addEventListener('activate', (event) => {
 	console.log('[Service Worker] Activating Service worker...', event);
+
+	event.waitUntil(
+		caches.keys().then((keyList) => {
+			return Promise.all(
+				keyList.map((key) => {
+					if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+						console.log('[Service Worker] Removing old cache: ', key);
+						return caches.delete(key);
+					}
+				})
+			);
+		})
+	);
+
 	return self.clients.claim();
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', (event) => {
 	// console.log('[Service Worker] Fetching something...', event);
 	// event.respondWith(fetch(event.request));
 
+	if (!(event.request.url.indexOf('http') === 0)) return; // dont cache for non-http requests
+
 	event.respondWith(
-		caches.match(event.request).then(function (response) {
+		caches.match(event.request).then((response) => {
 			if (response) {
 				return response;
 			} else {
-				return fetch(event.request);
+				return fetch(event.request).then((res) => {
+					return caches
+						.open(CACHE_DYNAMIC_NAME)
+						.then(function (cache) {
+							cache.put(event.request.url, res.clone());
+							return res;
+						})
+						.catch((err) => {});
+				});
 			}
 		})
 	);
